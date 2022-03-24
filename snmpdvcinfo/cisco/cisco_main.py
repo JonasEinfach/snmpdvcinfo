9b55e7953fc5b7f853491f206f3eb18abd1e425b
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-25 00:38:40 +0100

    Update cisco_main.py

--- snmpdvcinfo/cisco/cisco_main.py
+++ snmpdvcinfo/cisco/cisco_main.py
@@ -9,6 +9,14 @@
 
 # --------------- S T A R T   S C R I P T ---------------#
 
+# --------------- M O D U L E S ---------------#
+
+import cisco_sw_rt # --> import cisco functions
+
+# --------------- S N M P   O I D   V A R I A B L E S ---------------#
+
+OID_CISCO_VERSION = "1.3.6.1.2.1.1.1.0"
+
 # ------------------------------------------------------------------------------
 def get_cisco_main(ip,community_string):
 
@@ -17,13 +25,13 @@ def get_cisco_main(ip,community_string):
     if dvc_version != 0 and dvc_version[OID_CISCO_VERSION] != "": # --> check for snmp error
         dvc_version = dvc_version[OID_CISCO_VERSION]
 
-        if "NX-OS" in dvc_version: return get_dvc_info_cisco_nx(ip,community_string)
-        elif "Catalyst 4500 L3 Switch" in dvc_version: return get_dvc_info_cisco_c4500(ip,community_string)
-        elif "C1000" in dvc_version: return get_dvc_info_cisco_c1000(ip,community_string)
-        elif "C2960X" in dvc_version: return get_dvc_info_cisco_c2960x(ip,community_string)
-        elif "C3560" in dvc_version: return get_dvc_info_cisco_c3560(ip,community_string)
-        elif "s6t64" in dvc_version: return get_dvc_info_cisco_c6807(ip,community_string)
-        else: return get_dvc_info_cisco_default(ip,community_string)
+        if "NX-OS" in dvc_version: return cisco_sw_rt.get_dvc_info_cisco_nx(ip,community_string)
+        elif "Catalyst 4500 L3 Switch" in dvc_version: return cisco_sw_rt.get_dvc_info_cisco_c4500(ip,community_string)
+        elif "C1000" in dvc_version: return cisco_sw_rt.get_dvc_info_cisco_c1000(ip,community_string)
+        elif "C2960X" in dvc_version: return cisco_sw_rt.get_dvc_info_cisco_c2960x(ip,community_string)
+        elif "C3560" in dvc_version: return cisco_sw_rt.get_dvc_info_cisco_c3560(ip,community_string)
+        elif "s6t64" in dvc_version: return cisco_sw_rt.get_dvc_info_cisco_c6807(ip,community_string)
+        else: return cisco_sw_rt.get_dvc_info_cisco_default(ip,community_string)
 # ------------------------------------------------------------------------------
 
 #--------------- E N D   S C R I P T ---------------#

81898bab2ed79d332eb54838540a12aae0046c24
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-25 11:03:35 +0100

    Update snmpdvcinfo.py

--- snmpdvcinfo/snmpdvcinfo.py
+++ snmpdvcinfo/snmpdvcinfo.py
@@ -14,12 +14,6 @@
 
 from pysnmp import hlapi  # --> snmp requests
 
-from snmpdvcinfo import cisco # --> import cisco get functions
-from snmpdvcinfo import fortinet # --> import forti get functions
-
-from .snmp import get_snmp # --> import global snmp engine
-
-
 # --------------- G L O B A L   V A R I A B L E S ---------------#
 
 DEBUG = False
@@ -33,7 +27,7 @@ OID_GENERAL_DEVICE_INFO_2 = "1.3.6.1.2.1.1.1.0"
 def get_dvc_info(ip,community_string):
 
     vendor = ""
-    dvc_info = get_snmp(ip, [OID_GENERAL_DEVICE_INFO], hlapi.CommunityData(community_string))
+    dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO], hlapi.CommunityData(community_string))
 
     if dvc_info != 0 and dvc_info[OID_GENERAL_DEVICE_INFO] != "": # --> check for snmp error --> every device should have a sys description string
         dvc_info = dvc_info[OID_GENERAL_DEVICE_INFO]
@@ -41,7 +35,7 @@ def get_dvc_info(ip,community_string):
         vendor = get_vendor(dvc_info) # --> get vendor
 
     else:
-        dvc_info = get_snmp(ip, [OID_GENERAL_DEVICE_INFO_2], hlapi.CommunityData(community_string))
+        dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO_2], hlapi.CommunityData(community_string))
 
         if dvc_info != 0 and dvc_info[OID_GENERAL_DEVICE_INFO_2] != "": # --> check for snmp error --> every device should have a sys description string
             dvc_info = dvc_info[OID_GENERAL_DEVICE_INFO_2]
@@ -49,7 +43,7 @@ def get_dvc_info(ip,community_string):
             vendor = get_vendor(dvc_info) # --> get vendor
 
     if "Cisco" in vendor:
-        return cisco.cisco_main.get_cisco_main(ip,community_string)
+        return snmpdvcinfo.get_cisco_main(ip,community_string)
     else:
         return "none,none"
 # ------------------------------------------------------------------------------

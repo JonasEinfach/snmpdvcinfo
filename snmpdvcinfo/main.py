05e1f078a49a53d2b94390042c6e0d8f7bffc30b
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-25 11:24:44 +0100

    Update main.py

--- snmpdvcinfo/main.py
+++ snmpdvcinfo/main.py
@@ -12,7 +12,6 @@
 
 # --------------- M O D U L E S ---------------#
 
-from pysnmp import hlapi  # --> snmp requests
 import snmpdvcinfo
 
 # --------------- G L O B A L   V A R I A B L E S ---------------#
@@ -28,7 +27,7 @@ OID_GENERAL_DEVICE_INFO_2 = "1.3.6.1.2.1.1.1.0"
 def get_dvc_info(ip,community_string):
 
     vendor = ""
-    dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO], hlapi.CommunityData(community_string))
+    dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO], community_string)
 
     if dvc_info != 0 and dvc_info[OID_GENERAL_DEVICE_INFO] != "": # --> check for snmp error --> every device should have a sys description string
         dvc_info = dvc_info[OID_GENERAL_DEVICE_INFO]
@@ -36,7 +35,7 @@ def get_dvc_info(ip,community_string):
         vendor = get_vendor(dvc_info) # --> get vendor
 
     else:
-        dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO_2], hlapi.CommunityData(community_string))
+        dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO_2], community_string)
 
         if dvc_info != 0 and dvc_info[OID_GENERAL_DEVICE_INFO_2] != "": # --> check for snmp error --> every device should have a sys description string
             dvc_info = dvc_info[OID_GENERAL_DEVICE_INFO_2]

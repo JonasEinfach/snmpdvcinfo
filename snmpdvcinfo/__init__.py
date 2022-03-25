820d8a4830f60fbe11857edf434e4442cc30a8b4
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-25 11:29:32 +0100

    Update __init__.py

--- snmpdvcinfo/__init__.py
+++ snmpdvcinfo/__init__.py
@@ -2,4 +2,5 @@
 from .main import get_dvc_info
 from .snmp import *
 #vendor modules
-from cisco.cisco_main import *
+from snmpdvcinfo.cisco.cisco_main import *
+from snmpdvcinfo.cisco.cisco_sw_rt import *

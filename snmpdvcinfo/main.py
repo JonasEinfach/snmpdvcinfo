4daa6167aa29ce04ee1b6e5cd1c2fa3c58d1df32
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-25 11:53:38 +0100

    Update main.py

--- snmpdvcinfo/main.py
+++ snmpdvcinfo/main.py
@@ -20,37 +20,33 @@ DEBUG = False
 
 # --------------- S N M P   O I D   V A R I A B L E S ---------------#
 
-OID_GENERAL_DEVICE_INFO = "1.3.6.1.2.1.1.1"
-OID_GENERAL_DEVICE_INFO_2 = "1.3.6.1.2.1.1.1.0"
+OID_GENERAL_DEVICE_INFO = ["1.3.6.1.2.1.1.1","1.3.6.1.2.1.1.1.0","1.3.6.1.2.1.47.1.1.1.1.12.1"] # --> Cisco, Cisco, Forti
 
 # ------------------------------------------------------------------------------
 def get_dvc_info(ip,community_string):
-
-    vendor = ""
-    dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO], community_string)
-
-    if dvc_info != 0 and dvc_info[OID_GENERAL_DEVICE_INFO] != "": # --> check for snmp error --> every device should have a sys description string
-        dvc_info = dvc_info[OID_GENERAL_DEVICE_INFO]
-
-        vendor = get_vendor(dvc_info) # --> get vendor
-
-    else:
-        dvc_info = snmpdvcinfo.get_snmp(ip, [OID_GENERAL_DEVICE_INFO_2], community_string)
-
-        if dvc_info != 0 and dvc_info[OID_GENERAL_DEVICE_INFO_2] != "": # --> check for snmp error --> every device should have a sys description string
-            dvc_info = dvc_info[OID_GENERAL_DEVICE_INFO_2]
-
+     
+    for OID in OID_GENERAL_DEVICE_INFO:
+        
+        vendor = ""
+        dvc_info = snmpdvcinfo.get_snmp(ip, [OID], community_string)
+        
+        if dvc_info != 0 and dvc_info[OID] != "": # --> check for snmp error --> every device should have a sys description string
+            dvc_info = dvc_info[OID]
+            
             vendor = get_vendor(dvc_info) # --> get vendor
-
-    if "Cisco" in vendor:
-        return snmpdvcinfo.get_cisco_main(ip,community_string)
+            if "no_vendor_info" in vendor:
+                break
+        
+    
+    if "Cisco" in vendor: return snmpdvcinfo.get_cisco_main(ip,community_string)
+    elif: "Fortinet" in vendor: return "Fortinet,Fortinet"
     else:
         return "none,none"
 # ------------------------------------------------------------------------------
 def get_vendor(dvc_info):
 
-    if "Cisco" in dvc_info:
-        return "Cisco"
+    if "Cisco" in dvc_info: return "Cisco"
+    elif "Fortinet" in dvc_info: return "Fortinet"
 
     return "no_vendor_info"
 # ------------------------------------------------------------------------------

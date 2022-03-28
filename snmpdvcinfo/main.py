52b7ad8da2527de15481194faee1675a73f242a4
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-28 13:09:53 +0200

    Update main.py

--- snmpdvcinfo/main.py
+++ snmpdvcinfo/main.py
@@ -41,7 +41,7 @@ def get_dvc_info(ip,community_string):
     if   "Cisco" in vendor:    return snmpdvcinfo.get_cisco_main(ip,community_string)
     elif "Fortinet" in vendor: return snmpdvcinfo.get_fortinet_main(ip,community_string)
     else:
-        return "none,none"
+        return "none,none,none,none"
 # ------------------------------------------------------------------------------
 def get_vendor(dvc_info):
 

33d9384bf7b42e19257f71d3ba81c672ef513aa1
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-03-30 12:05:07 +0200

    Update cisco_main.py

--- snmpdvcinfo/cisco/cisco_main.py
+++ snmpdvcinfo/cisco/cisco_main.py
@@ -31,6 +31,7 @@ def get_cisco_main(ip,community_string):
         elif "C2960X" in dvc_version:                   return snmpdvcinfo.get_dvc_info_cisco_c2960x(ip,community_string)
         elif "C3560" in dvc_version:                    return snmpdvcinfo.get_dvc_info_cisco_c3560(ip,community_string)
         elif "s6t64" in dvc_version:                    return snmpdvcinfo.get_dvc_info_cisco_c6807(ip,community_string)
+        elif "Adaptive Security Appliance" in dvc_version: return "cisco,fw_not_supported,fw_not_supported,fw_not_supported"
         else:                                           return snmpdvcinfo.get_dvc_info_cisco_default(ip,community_string)
 # ------------------------------------------------------------------------------
 

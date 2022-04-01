8bf8a873f5c15c861967d34d21b8e11f392f4daa
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2022-04-01 13:53:50 +0200

    Update fortinet_fw.py

--- snmpdvcinfo/fortinet/fortinet_fw.py
+++ snmpdvcinfo/fortinet/fortinet_fw.py
@@ -21,6 +21,8 @@ OID_FORTINET_VERSION = "1.3.6.1.2.1.47.1.1.1.1.10.1"
 
 OID_FORTINET_SN = "1.3.6.1.2.1.47.1.1.1.1.11.1"
 
+OID_FORTINET_HOSTNAME = "1.3.6.1.2.1.1.5.0"
+
 # ------------------------------------------------------------------------------
 def get_dvc_info_fortinet_default(ip,community_string): # --> get model and version for fortinet devices --> default snmp getter
     
@@ -52,6 +54,14 @@ def get_dvc_info_fortinet_default(ip,community_string): # --> get model and vers
     if dvc_sn != 0 and dvc_sn[OID_FORTINET_SN] != "": # --> check for snmp error
         # --> extract Version aus Dict
         final_dvc_sn = dvc_sn[OID_FORTINET_SN]
+    
+    # --> get hostname for default 
+    dvc_hostname = snmpdvcinfo.get_snmp(ip, [OID_FORTINET_HOSTNAME], community_string)
+
+    if dvc_hostname != 0 and dvc_hostname[OID_FORTINET_HOSTNAME] != "": # --> check for snmp error
+        # --> extract Version aus Dict
+        final_dvc_hostname = dvc_hostname[OID_FORTINET_HOSTNAME]   
+    
 
     return "%s,%s,%s,%s,%s" % (final_dvc_vendor,final_dvc_hostname,final_dvc_model,final_dvc_version,final_dvc_sn)
 # ------------------------------------------------------------------------------

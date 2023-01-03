60b384d35856d61a3e8b28e80409d97eb249fe57
Einfach Jonas <100405740+JonasEinfach@users.noreply.github.com>
2023-01-03 17:35:12 +0100

    Update __init__.py

--- snmpdvcinfo/__init__.py
+++ snmpdvcinfo/__init__.py
@@ -4,5 +4,6 @@ from .snmp import *
 #vendor modules
 from snmpdvcinfo.cisco.cisco_main import *
 from snmpdvcinfo.cisco.cisco_sw_rt import *
+from snmpdvcinfo.cisco.cisco_wlc import *
 from snmpdvcinfo.fortinet.fortinet_main import *
 from snmpdvcinfo.fortinet.fortinet_fw import *
